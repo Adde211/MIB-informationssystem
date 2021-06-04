815c16f5f0c61c7c55fe7ae50dde37138713595f
AxlLarsson <83978761+AxlLarsson@users.noreply.github.com>
2021-06-04 13:26:23 +0200

    Revert "Revert "Uppdatering""
    This reverts commit 5eec2e29dfd430236c5434d81a024027b8cb8507.

--- src/MIB_informationssystem/UppdateraEnAlien.java
+++ src/MIB_informationssystem/UppdateraEnAlien.java
@@ -72,6 +72,7 @@ public class UppdateraEnAlien extends javax.swing.JFrame {
         jLabel11 = new javax.swing.JLabel();
         lblFelmeddelandeNamn = new javax.swing.JLabel();
         lblFelmeddelandeID = new javax.swing.JLabel();
+        btnExit = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -132,6 +133,13 @@ public class UppdateraEnAlien extends javax.swing.JFrame {
         jLabel4.setText("jLabel3");
 
         jLabel11.setText("Ansvarig agent");
+
+        btnExit.setText("Exit");
+        btnExit.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnExitActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -201,7 +209,9 @@ public class UppdateraEnAlien extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addGap(389, 389, 389)
                 .addComponent(btnUppdatera)
-                .addGap(0, 0, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(btnExit, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(53, 53, 53))
             .addGroup(layout.createSequentialGroup()
                 .addGap(29, 29, 29)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 370, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -260,7 +270,9 @@ public class UppdateraEnAlien extends javax.swing.JFrame {
                     .addComponent(tfRegistreringsdatum, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel14))
                 .addGap(66, 66, 66)
-                .addComponent(btnUppdatera)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnUppdatera)
+                    .addComponent(btnExit))
                 .addContainerGap(39, Short.MAX_VALUE))
         );
 
@@ -402,6 +414,13 @@ public class UppdateraEnAlien extends javax.swing.JFrame {
         }
     }//GEN-LAST:event_tfNamnActionPerformed
 
+    private void btnExitActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnExitActionPerformed
+        // TODO add your handling code here:
+        
+        new AdminUtomjordingsmeny().setVisible(true);
+        dispose();
+    }//GEN-LAST:event_btnExitActionPerformed
+
     
     
     /**
@@ -441,6 +460,7 @@ public class UppdateraEnAlien extends javax.swing.JFrame {
     
     
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnExit;
     private javax.swing.JButton btnUppdatera;
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
